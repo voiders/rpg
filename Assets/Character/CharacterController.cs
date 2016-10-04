51a56f8deafe8d5473a334accc2ad8996138d89e
Jose Albizures <albizures3601@gmail.com>
2016-10-04 03:31:45 -0600

    se creo un mapa de pruebas

--- Assets/Character/CharacterController.cs
+++ Assets/Character/CharacterController.cs
@@ -6,12 +6,14 @@ public class CharacterController : MonoBehaviour {
   public Character character;
   public float speed = 5;
   private Animator anim;
+  private Rigidbody2D myRigibody;
   private bool moving;
   private Vector2 lastMove;
   // Use this for initialization
   void Start () {
     anim = GetComponent<Animator>();
     character = new Character();
+    myRigibody = GetComponent<Rigidbody2D>();
   }
   
   // Update is called once per frame
@@ -22,26 +24,34 @@ public class CharacterController : MonoBehaviour {
 
     
     if (xAxis != 0f){
-      transform.Translate(
-        new Vector3(xAxis * Time.deltaTime * speed, 0f, 0f)
-      );
+      // transform.Translate(
+      //   new Vector3(xAxis * Time.deltaTime * speed, 0f, 0f)
+      // );
+      myRigibody.velocity = new Vector2(xAxis * speed, myRigibody.velocity.y);
       lastMove.Set(
         xAxis,
         0f
       );
       moving = true;
-    } 
+    } else {
+      myRigibody.velocity = new Vector2(0f, myRigibody.velocity.y);
+    }
     
     if(yAxis != 0f) {
-      transform.Translate(
-        new Vector3(0f, yAxis * Time.deltaTime * speed, 0f)
-      );
+      // transform.Translate(
+      //   new Vector3(0f, yAxis * Time.deltaTime * speed, 0f)
+      // );
+      myRigibody.velocity = new Vector2(myRigibody.velocity.x, yAxis * speed);
       lastMove.Set(
         0f,
         yAxis
       );
       moving = true;
+    } else {
+      myRigibody.velocity = new Vector2(myRigibody.velocity.x, 0f);
     }
+
+
     anim.SetFloat("MoveX", xAxis);
     anim.SetFloat("MoveY", yAxis);
     anim.SetFloat("LastMoveX", lastMove.x);
